909647b10256e86131d3b4bd4d9dd4be65a3dd8e
niomion <nio.niomion@gmail.com>
2024-04-26 22:40:35 +0300

    4commit

--- js/task1.js
+++ js/task1.js
@@ -14,3 +14,45 @@ function Change(page) {
         });
     }, 200);
 };
+
+//
+
+function createList() {
+    var namef = prompt('Enter you name','');
+    var position = prompt('Enter your position','');
+    var phone = prompt('Enter your phone','');
+    var visitcard = ' <TABLE width="100%" border="0">'
++' <TR>'
++' <TD align="CENTER" valign="CENTER">'
++' <IMG width="100px" height="100px"  src="../pineapple.webp">'
++' </TD>'
++' <TD align="CENTER">'
++' <B>' + namef + '</B><BR>'
++' <I>' + position + '</I><BR>'
++' <FONT color="blue">' + phone + '</FONT>'
++' </TD>'
++' </TR>'
++' </TABLE>';
+
+var containerDiv = document.createElement('div');
+containerDiv.className = 'put'; 
+var table = document.createElement('table');
+table.setAttribute('width', '100%');
+table.setAttribute('border', '0');
+table.setAttribute('align', 'center');
+for (var y = 0; y < 12; y++) {
+    var row = document.createElement('tr');
+    for (var x = 0; x < 3; x++) {
+        var cell = document.createElement('td');
+        cell.setAttribute('align', 'center');
+        cell.style.borderStyle = 'dashed';
+        cell.style.borderWidth = 'thin';
+        cell.innerHTML = visitcard; 
+        row.appendChild(cell);
+    }
+    table.appendChild(row);
+}
+containerDiv.appendChild(table);
+document.body.appendChild(containerDiv);
+}
+
--- js/task3.js
+++ js/task3.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".loading4").slideUp(200);
+
 });
 
 function Change(page) {
@@ -14,3 +15,22 @@ function Change(page) {
         });
     }, 200);
 };
+
+function showMath() {
+
+    var i1 = 1;
+    let containerDiv = document.createElement('div');
+    let array = [Math.E, Math.PI, Math.log10(Math.E), Math.log2(Math.E), Math.log(10), Math.log(2), Math.sqrt(2), Math.sqrt(1/2),
+                 findTan(45), Math.ceil(1.00123), Math.ceil(-3.1234), Math.floor(1.00123), Math.random()];
+    array.forEach(element => {
+        containerDiv.innerHTML += "<p>" + i1++ + ") Result: " + element + "</p>";
+    });
+    document.body.appendChild(containerDiv);
+}
+
+function findTan(degrees) {
+    let radian = degrees * (Math.PI / 180);
+    let tan = Math.tan(radian);
+    return Math.round(tan);
+}
+
